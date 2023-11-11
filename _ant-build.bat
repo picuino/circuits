4839a354f23160e2e714ba9a459d159788e9fbf8
Carlos Pardo <carlos@picuino.com>
2023-11-11 18:49:19 +0100

    Actualizado a 11/11/2023

--- _ant-build.bat
+++ _ant-build.bat
@@ -1,8 +1,8 @@
 @echo off
 set PATH=\Bin\ant\bin;%PATH%
 set PATH=\Bin\cygwin64\bin;%PATH%
-set PATH=\Bin\jdk180\bin;%PATH%
-set JAVA_HOME=\Bin\jdk180\bin
+set PATH=\Bin\jdk210\bin;%PATH%
+set JAVA_HOME=\Bin\jdk210\bin
 call ant.bat build
 rm -f docs/*.cache.js
 cp -p war/circuitjs1/*.js docs
